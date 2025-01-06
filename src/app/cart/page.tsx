65fc1d7e7a23c5e4ac10bcaae779b198e63394b0
Flavio Ochoa <flavio.ochoa@dxc.com>
2025-01-05 19:19:49 -0600

    test(0004-final-review): fix issue with config

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,5 +1,4 @@
 import dynamic from "next/dynamic";
-import Link from "next/link";
 import { BreadcrumbLayout, Breadcrumb } from "@/components/breadcrumb";
 
 const CartPage = dynamic(
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,4 @@
 import CatalogPage from "@/components/pages/catalog-page/catalog-page";
-import { GameRequestParams, getGamesBy } from "@/services/games.service";
-
-interface HomeProps {
-  searchParams: GameRequestParams;
-}
 
 export default async function Home() {
   return (
